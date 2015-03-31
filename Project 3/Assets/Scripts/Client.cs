7972149731cac2e6075d3ca31e1691d36a503f0d
MichaelKarnes <mkarnes2015@gmail.com>
2015-03-30 22:50:40 -0500

    Project 3 Server/Client Update

--- Project 3/Assets/Scripts/Client.cs
+++ Project 3/Assets/Scripts/Client.cs
@@ -3,9 +3,9 @@ using System.Collections;
 using UnityEngine.UI;
 
 public class Client : MonoBehaviour {
-	public string IP = "127.0.0.1";
-	public int Port = 25001;
-	
+	private string IP;
+	private int Port;
+
 	// Use this for initialization
 	void Start () {
 	}
@@ -13,17 +13,32 @@ public class Client : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (Network.peerType != NetworkPeerType.Disconnected) {
-			transform.Find ("IP Address").GetComponent<Text> ().text = "Server IP Address: "+IP;
-			transform.Find ("Port").GetComponent<Text> ().text = "Port: "+Port;
-			if(Network.connections.Length > 0)
+			if(Network.connections.Length > 0) {
 				transform.Find ("Connection").gameObject.SetActive (true);
-			else
+				transform.Find ("IP Address Box").gameObject.SetActive (false);
+				transform.Find ("IP Address").gameObject.SetActive (true);
+				transform.Find ("Port Box").gameObject.SetActive (false);
+				transform.Find ("Port").gameObject.SetActive (true);
+				transform.Find ("Connect").gameObject.SetActive (false);
+			}
+			else {
 				transform.Find ("Connection").gameObject.SetActive (false);
+				transform.Find ("IP Address Box").gameObject.SetActive (true);
+				transform.Find ("IP Address").gameObject.SetActive (false);
+				transform.Find ("Port Box").gameObject.SetActive (true);
+				transform.Find ("Port").gameObject.SetActive (false);
+				transform.Find ("Connect").gameObject.SetActive (true);
+			}
 		}
 	}
 
 	void Connect() {
-		if (Network.peerType == NetworkPeerType.Disconnected)
-			Network.Connect (IP, Port);
+		if (Network.peerType != NetworkPeerType.Disconnected)
+			return;
+		IP = transform.Find ("IP Address Box").Find ("Text").GetComponent<Text> ().text;
+		Port = int.Parse (transform.Find ("Port Box").Find ("Text").GetComponent<Text> ().text);
+		transform.Find ("IP Address").GetComponent<Text> ().text = "IP Address: "+IP;
+		transform.Find ("Port").GetComponent<Text> ().text = "Port: "+Port;
+		Network.Connect (IP, Port);
 	}
 }
--- Project 3/Assets/Scripts/Server.cs
+++ Project 3/Assets/Scripts/Server.cs
@@ -1,23 +1,36 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
 
 public class Server : MonoBehaviour {
-	public string IP = "127.0.0.1";
-	public int Port = 25001;
+	private string IP = "127.0.0.1";
+	private int Port = 25001;
 
 	// Use this for initialization
 	void Start () {
-		Network.InitializeServer (10, Port);
+		IP = Network.player.ipAddress;
+		transform.Find ("IP Address").GetComponent<Text> ().text = "IP Address: "+IP;
 	}
 	
 	// Update is called once per frame
-	void OnGUI () {
-		transform.Find ("IP Address").GetComponent<Text> ().text = "IP Address: "+IP;
-		transform.Find ("Port").GetComponent<Text> ().text = "Port: "+Port;
+	void Update () {
 		if(Network.connections.Length > 0)
 			transform.Find ("Connection").gameObject.SetActive (true);
 		else
 			transform.Find ("Connection").gameObject.SetActive (false);
+	}
+
+	void Create() {
+		if (Network.peerType != NetworkPeerType.Disconnected)
+			return;
+		Port = int.Parse (transform.Find ("PortBox").Find ("Text").GetComponent<Text> ().text);
+		transform.Find ("PortBox").gameObject.SetActive (false);
+		transform.Find ("Port").GetComponent<Text> ().text = "Port: " + Port;
+		transform.Find ("Port").gameObject.SetActive (true);
+		Destroy (transform.Find ("Create").gameObject);
+		Network.InitializeServer (10, Port);
 	}
 }
