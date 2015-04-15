61daf9d0583dc14a7ff8410a5100143331ad0851
MichaelKarnes <mkarnes2015@gmail.com>
2015-04-15 15:14:26 -0500

    Project 4 Buggy Demo

---
+++ Project 4/Assets/Scripts/Buggy.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Buggy : MonoBehaviour {
+	public WheelCollider LeftWheel;
+	public WheelCollider RightWheel;
+	public Transform LeftSensor;
+	public Transform RightSensor;
+	public float[,] K;
+	private Light[] lights;
+
+	// Use this for initialization
+	void Start () {
+		K = new float[2, 2]{{-.9f,1},{1,-.9f}};
+		lights = FindObjectsOfType (typeof(Light)) as Light[];
+	}
+
+	float DistanceToLight(Transform sensor, Light light) {
+		return Vector3.Magnitude (light.transform.position - sensor.position);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//LeftWheel.motorTorque = 1f;
+		//RightWheel.motorTorque = .5f;
+		LeftWheel.motorTorque = 0f;
+		RightWheel.motorTorque = 0f;
+		foreach (Light light in lights) {
+			LeftWheel.motorTorque += K[0,0]*100/DistanceToLight (LeftSensor, light) + K[1,0]*100/DistanceToLight (RightSensor, light);
+			RightWheel.motorTorque += K[0,1]*100/DistanceToLight (LeftSensor, light) + K[1,1]*100/DistanceToLight (RightSensor, light);
+		}
+		LeftWheel.transform.Find ("Tire_L").Rotate (LeftWheel.rpm / 60 * 360 * Time.deltaTime,0,0);
+		RightWheel.transform.Find ("Tire_R").Rotate (RightWheel.rpm / 60 * 360 * Time.deltaTime,0,0);
+	}
+}
---
+++ Project 4/Assets/Scripts/Camera.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButton(0)) {
+			transform.Translate(Vector3.right * -Input.GetAxis("Mouse X") * 1f);
+			transform.Translate(transform.up * -Input.GetAxis("Mouse Y") * 1f, Space.World);
+		}
+		if (Input.GetAxis ("Mouse ScrollWheel") != 0) {
+			transform.Translate (transform.up * Input.GetAxis ("Mouse ScrollWheel") * 10f);
+		}
+	}
+}
