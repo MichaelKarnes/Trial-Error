d168b0f1f0da5f84544bbcfd5a744901ff02ce63
MichaelKarnes <mkarnes2015@gmail.com>
2015-04-15 19:06:17 -0500

    Project 4 Buggy Demo Basic Functionality

--- Project 4/Assets/Scripts/Buggy.cs
+++ Project 4/Assets/Scripts/Buggy.cs
@@ -2,29 +2,39 @@
 using System.Collections;
 
 public class Buggy : MonoBehaviour {
+	public Transform transform;
 	public WheelCollider LeftWheel;
 	public WheelCollider RightWheel;
 	public Transform LeftSensor;
 	public Transform RightSensor;
 	public float[,] K;
 	private Light[] lights;
+	private bool on = false;
+
+	public void Init() {
+		K = new float[2, 2]{{-.5f,1},{1,-.5f}};
+		lights = FindObjectsOfType (typeof(Light)) as Light[];
+	}
 
 	// Use this for initialization
 	void Start () {
-		K = new float[2, 2]{{-.9f,1},{1,-.9f}};
-		lights = FindObjectsOfType (typeof(Light)) as Light[];
+		Init ();
 	}
 
 	float DistanceToLight(Transform sensor, Light light) {
 		return Vector3.Magnitude (light.transform.position - sensor.position);
 	}
+
+	public void Activate() {
+		on = true;
+	}
 	
 	// Update is called once per frame
 	void Update () {
-		//LeftWheel.motorTorque = 1f;
-		//RightWheel.motorTorque = .5f;
 		LeftWheel.motorTorque = 0f;
 		RightWheel.motorTorque = 0f;
+		if (!on)
+			return;
 		foreach (Light light in lights) {
 			LeftWheel.motorTorque += K[0,0]*100/DistanceToLight (LeftSensor, light) + K[1,0]*100/DistanceToLight (RightSensor, light);
 			RightWheel.motorTorque += K[0,1]*100/DistanceToLight (LeftSensor, light) + K[1,1]*100/DistanceToLight (RightSensor, light);
