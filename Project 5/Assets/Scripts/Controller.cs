5ad73cbc39e4c2cd97845228f2b405f389ec469e
MichaelKarnes <mkarnes2015@gmail.com>
2015-04-30 01:08:45 -0500

    Project 5 Final

--- Project 5/Assets/Scripts/Controller.cs
+++ Project 5/Assets/Scripts/Controller.cs
@@ -5,12 +5,14 @@ using System.Collections.Generic;
 public class Controller : MonoBehaviour {
 	private bool _isMoving = false;
 	private List<Vector3> _path = new List<Vector3>();
-	private List<Vector3> _points = new List<Vector3> ();
+	private List<Node> _nodes = new List<Node> ();
 	private float _start;
 	private float speed = 5.0f;
+	private TrailRenderer trail;
 
 	// Use this for initialization
 	void Start () {
+		trail = GetComponent<TrailRenderer> ();
 		/*_path.Add (new Vector3 (-5, 0.25f, 5));
 		_path.Add (new Vector3 (5, 0.25f, 5));
 		_path.Add (new Vector3 (-5, 0.25f, -5));
@@ -36,24 +38,138 @@ public class Controller : MonoBehaviour {
 		}
 		if (Input.GetMouseButtonDown (1)) {
 			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-			Plane xzPlane = new Plane(Vector3.up, Vector3.zero);
+			Plane xzPlane = new Plane(Vector3.up, new Vector3(0, 0.25f, 0));
 			float distance = 0;
 			if (xzPlane.Raycast(ray, out distance))
 				MoveTo(new Vector3(ray.GetPoint(distance).x, transform.position.y, ray.GetPoint (distance).z));
 		}
 	}
 
+	static IEnumerator ResetTrail(TrailRenderer trail) {
+		trail.time = 0;
+		yield return 0;
+		trail.time = float.PositiveInfinity;
+	}
+
 	private void MoveTo(Vector3 q) {
 		Vector3 p = transform.position;
-		_points = new List<Vector3> ();
-		_points.Add (p);
-		_points.AddRange (CellDivisionPoints (GameObject.Find ("Building 1")));
-		_points.AddRange (CellDivisionPoints (GameObject.Find ("Building 2")));
-		_points.AddRange (CellDivisionPoints (GameObject.Find ("Building 3")));
-		_points.Add (q);
+		List<Vector3> points = new List<Vector3> ();
+		points.AddRange (CellDivisionPoints (GameObject.Find ("Building 1")));
+		points.AddRange (CellDivisionPoints (GameObject.Find ("Building 2")));
+		points.AddRange (CellDivisionPoints (GameObject.Find ("Building 3")));
+		_nodes = new List<Node> ();
+		foreach (Vector3 point in points) {
+			Node node = new Node (point);
+			foreach(Node n in _nodes) {
+				n.neighborNodes.Add (node);
+				node.neighborNodes.Add (n);
+			}
+			_nodes.Add (node);
+		}
+		Node start = new Node (p);
+		foreach(Node n in _nodes) {
+			n.neighborNodes.Add (start);
+			start.neighborNodes.Add (n);
+		}
+		_nodes.Add (start);
+		Node goal = new Node (q);
+		foreach(Node n in _nodes) {
+			n.neighborNodes.Add (goal);
+			goal.neighborNodes.Add (n);
+		}
+		_nodes.Add (goal);
+		foreach(Node n in _nodes) {
+			print (n.nodePos);
+		}
+		_nodes = AStarSearch (start, goal);
 		_path = new List<Vector3> ();
-		//_start = Time.time;
-		//_isMoving = true;
+		foreach (Node n in _nodes)
+			_path.Add (n.nodePos);
+		//_path.AddRange (_points);
+		StartCoroutine(ResetTrail(trail));
+		_start = Time.time;
+		_isMoving = true;
+	}
+
+	public List<Node> AStarSearch( Node p, Node q )
+	{
+		List<Node> closedList = new List<Node>();
+		List<Node> openList = new List<Node>();
+		openList.Add (p);
+		
+		p.setActualCostG (0);
+		p.setF(p.getActualCost() + Vector3.Distance(p.nodePos, q.nodePos));
+		while (openList.Count !=0) 
+		{
+			Node current = openList[0];
+			
+			for(int i = 1; i<openList.Count; i++)
+			{
+				if(openList[i].getFvalue() < current.getFvalue())
+				{
+					current = openList[i];
+				}
+			}
+			if(current == q)
+			{
+				return reconstructPath(q);
+			}
+			
+			
+			openList.Remove(current);
+			closedList.Add(current); // Att the begginning we have Start and we move it to the closed list.
+			
+			foreach(Node n in current.neighborNodes)
+			{
+				if (closedList.Contains(n))
+					continue;
+				float estimatedGScore = current.getActualCost() + Vector3.Distance(current.nodePos, n.nodePos);
+				if(hitSomething(current.nodePos, n.nodePos))
+				{
+					estimatedGScore = float.PositiveInfinity;
+				}
+				
+				if(!openList.Contains(n) || (estimatedGScore < n.getActualCost()))
+				{
+					n.setParentNode(current);
+					n.setActualCostG(estimatedGScore);
+					n.setF(n.getActualCost() + Vector3.Distance(n.nodePos, q.nodePos));
+					if(!openList.Contains(n))
+					{
+						openList.Add(n);
+					}
+				}
+			}
+			
+			
+		}
+		
+		return new List<Node>();
+	}
+	
+	public List<Node> reconstructPath( Node q)
+	{
+		List<Node> totalPath = new List<Node>();
+		totalPath.Add (q);
+		Node current = q;
+		
+		while (current.getParent() != null) 
+		{
+			totalPath.Add(current.getParent());
+			current = current.getParent();
+		}
+		
+		totalPath.Reverse ();
+		return totalPath;
+	}
+
+	private bool hitSomething(Vector3 origin, Vector3 destination)
+	{
+		RaycastHit hit;
+		if(Physics.Raycast(origin, (destination-origin), Vector3.Distance(destination, origin)))
+			return true;
+		else
+			return false;
 	}
 
 	private List<Vector3> CellDivisionPoints(GameObject obstacle) {
@@ -92,10 +208,12 @@ public class Controller : MonoBehaviour {
 	}
 
 	private Vector3 CellDivisionPoint(GameObject obstacle, Vector3 origin, Vector3 direction) {
+		if (origin.x < -5f || origin.x > 5f || origin.z < -5f || origin.z > 5f)
+			return Vector3.zero;
 		RaycastHit hit;
 		if (Physics.Raycast (new Ray (origin, direction), out hit, direction.z > 0 ? 5f + origin.z : 5f - origin.z)) {
 			if (hit.transform.gameObject != obstacle)
-				return new Vector3 (origin.x, 0, (hit.point.z + origin.z) / 2);
+				return new Vector3 (origin.x, .25f, (hit.point.z + origin.z) / 2);
 		} else {
 			if (GameObject.Find ("Building 1") != obstacle && GameObject.Find ("Building 1").GetComponent<BoxCollider> ().bounds.Contains (origin))
 				return origin;
@@ -103,7 +221,7 @@ public class Controller : MonoBehaviour {
 				return origin;
 			if (GameObject.Find ("Building 3") != obstacle && GameObject.Find ("Building 3").GetComponent<BoxCollider> ().bounds.Contains (origin))
 				return origin;
-			return new Vector3 (origin.x, 0, direction.z > 0 ? (5f + origin.z) / 2 : (-5f + origin.z) / 2);
+			return new Vector3 (origin.x, .25f, direction.z > 0 ? (5f + origin.z) / 2 : (-5f + origin.z) / 2);
 		}
 		return Vector3.zero;
 	}
---
+++ Project 5/Assets/Scripts/Node.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Node {
+
+
+	//Transform objectTransform = new Transform(); 
+	public Vector3 nodePos = new Vector3(0,0,0);
+	public List<Node> neighborNodes = new List<Node>();
+	Node parentNode;
+	float actualCostG = 0;
+	float estimatedCostH = 0;
+	float fResult = 0;
+
+	public Node()
+	{
+		 nodePos = new Vector3(0,0,0);
+
+		 actualCostG = 0;
+		 estimatedCostH = 0;
+		 fResult = 0;
+	}
+
+	public Node(Vector3 p)
+	{
+		nodePos = p;
+		
+		actualCostG = 0;
+		estimatedCostH = 0;
+		fResult = 0;
+	}
+	// Use this for initialization
+	void Start () {
+
+		//nodePos.x = transform.position.x;
+		//nodePos.y = transform.position.y;
+		//nodePos.z = transform.position.z;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	
+	public void outputPosition() {
+	
+		//Debug.Log ("TEST");
+		//Debug.Log (nodePos.x);
+	}
+
+	public void setNodePos(Vector3 pos)
+	{
+		nodePos = pos;
+	}
+
+
+	public void setParentNode(Node n)
+	{
+		parentNode = n;
+	}
+	public void setActualCostG(float actualCost)
+	{
+		actualCostG = actualCost;
+	}
+	public void setEstimatedCostH(float estimatedCost)
+	{
+		estimatedCostH = estimatedCost;
+	}
+	public void setF(float f)
+	{
+		fResult = f;
+	}
+	public float getActualCost()
+	{
+		return actualCostG;
+	}
+	public float getEstimatedCost()
+	{
+		return estimatedCostH;
+	}
+	public float getFvalue()
+	{
+		return fResult;
+	}
+	public Node getParent()
+	{
+		return parentNode;
+	}
+}
